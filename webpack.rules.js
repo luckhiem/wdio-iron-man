a4e25e676cfdac3ded544978a65e9cd12ff56865
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-09-27 15:49:16 +0700

    update

--- webpack.rules.js
+++ webpack.rules.js
@@ -43,8 +43,8 @@ module.exports = [
       options: {
         lessOptions: { // If you are using less-loader@5 please spread the lessOptions to options directly
           modifyVars: {
-            'primary-color': '#1DA57A',
-            'link-color': '#1DA57A',
+            'primary-color': '#00b14f',
+            'link-color': '#00b14f',
             'border-radius-base': '2px',
           },
           javascriptEnabled: true,
